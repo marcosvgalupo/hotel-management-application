793c1b3e0f651264df5ac394a9ea65058063e9a5
Giovana Nogueira <ongiovana2016@gmail.com>
2023-12-06 11:35:06 -0300

    added new css style

--- src/main/java/br/com/unifalmg/hotel/service/ManagerService.java
+++ src/main/java/br/com/unifalmg/hotel/service/ManagerService.java
@@ -1,8 +1,11 @@
 package br.com.unifalmg.hotel.service;
 
 import br.com.unifalmg.hotel.entity.Manager;
+import br.com.unifalmg.hotel.entity.RoomType;
 import br.com.unifalmg.hotel.exception.InvalidDataBaseConnection;
 import br.com.unifalmg.hotel.exception.InvalidManagerException;
+import br.com.unifalmg.hotel.exception.ManagerNotFoundException;
+import br.com.unifalmg.hotel.exception.RoomTypeNotFoundException;
 import br.com.unifalmg.hotel.repository.ManagerRepository;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
@@ -29,6 +32,15 @@ public class ManagerService {
             return repository.findAll();
         }
 
+    public Manager findById(Integer id){
+        if(Objects.isNull(id)){
+            throw new IllegalArgumentException("Null ID when fetching for a manager.");
+        }
+        return repository.findById(id).orElseThrow(
+                () -> new ManagerNotFoundException(String.format("No manager found for id %d", id))
+        );
+    }
+
         public boolean autenticate(String username, String password){
            if(username != null && password != null){
                Manager manager = repository.findByUsernameAndPassword(username, password);
@@ -41,6 +53,10 @@ public class ManagerService {
 
     public List<Object[]> employeeAndManager() {
         return repository.employeeAndManager();
+    }
+
+    public void updateManager(String username, String password) {
+        repository.updateManager(username, password);
     }
 
 
